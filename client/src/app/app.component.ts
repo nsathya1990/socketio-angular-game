09ec43c39bc633d757970340a48af4f6ae438ef8
unknown <nsathya1990@gmail.com>
2021-02-20 13:08:36 +0530

    add canvas

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit, AfterViewInit {
+  private context: any;
+  @ViewChild('game') private gameCanvas: ElementRef;
+
+  // Establishes connection with our socket server
+  ngOnInit(): void {}
+
+  // After our HTML renders, we can do interaction with our canvas
+  ngAfterViewInit(): void {
+    // getContext is a function that exist as part of the canvas
+    this.context = this.gameCanvas.nativeElement.getContext("2d");
+    // let's add a rectangle
+    this.context.fillRect(20, 20, 20, 20);
+  }
 }
