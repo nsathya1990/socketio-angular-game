e29788597f450ece67a726e77442c6c3fd773737
unknown <nsathya1990@gmail.com>
2021-02-20 13:39:00 +0530

    establishing socketio connection at server

--- server/app.js
+++ server/app.js
@@ -4,6 +4,18 @@ const Socketio = require('socket.io')(Http);
 // socketio will do all our communication with our socket
 // express will serve that web socket for us
 
+const position = {
+  x: 200,
+  y: 200,
+};
+
+// The connection event is reserved event by socketio. This event is triggered every time a new socket connects to this server
+// When a socket connects, we'll have information about that particular socket
+// In this case, we want to share the position information with them.
+Socketio.on('connection', socket => {
+    socket.emit("position", position);
+});
+
 Http.listen(3000, () => {
   console.log('Listening at port 3000 ...');
 });
